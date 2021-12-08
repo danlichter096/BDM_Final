472e067bf711370a625123edcf3d9e1252c9ceeb
danlichter096 <52501911+danlichter096@users.noreply.github.com>
2021-12-08 13:20:22 -0500

    Update BDM_HW4_df.py

--- BDM_HW4_df.py
+++ BDM_HW4_df.py
@@ -72,7 +72,7 @@ def main(sc, spark):
 
     dfI = dfH.groupBy('group', 'year', 'date') \
             .agg(F.collect_list('visits').alias('visits')) \
-            .withColumn('stats', udfComputeStats('group', 'visits')).select('group','year','date')
+            .withColumn('stats', udfComputeStats('group', 'visits')).select('group','year','date', 'stats.*')
     dfI.write.csv(f'{OUTPUT_PREFIX}/test',mode='overwrite', header=True)
     
    
